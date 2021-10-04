92b3cae50d6e4c265dbc7bd8ef93e529ccc9b4bb
Nikita Barinov <nikitabarinov2301@gmail.com>
2021-10-04 15:58:29 +0400

    feat(types): prototype Storage utility type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,8 @@ export type Nullable<T> = T | null;
 export type AnyObject = Record<string, any>;
 
 export type AnyFunction = (...args: any[]) => any;
+
+export interface Storage<Entity extends AnyObject, ID extends keyof any> {
+  ids: Nullable<ID[]>;
+  storage: Record<ID, Entity>;
+}
