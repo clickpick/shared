a15e7d69c4ff97d4e1145ca91eb2a57bef9eff2c
Nikita Barinov <nikitabarinov2301@gmail.com>
2021-11-30 18:41:35 +0400

    fix!: add exports string & type-checkers utils

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from './react';
 export * from './classNames';
 export * from './dom';
 export * from './env';
+export * from './string';
+export * from './type-checkers';
 export * from './types';
